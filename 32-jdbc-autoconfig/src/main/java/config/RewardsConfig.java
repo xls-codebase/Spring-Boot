d20b7b2897fb332d77e35ae035513f79801e2a1b
xls-codebase <93859139+xls-codebase@users.noreply.github.com>
2024-10-10 22:29:53 +0200

    Spring Boot - A Closer Look TODO-10 (Optional): Switch to explicit DataSource configuration

--- 32-jdbc-autoconfig/src/main/java/config/RewardsConfig.java
+++ 32-jdbc-autoconfig/src/main/java/config/RewardsConfig.java
@@ -2,9 +2,9 @@ package config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import rewards.RewardNetwork;
 import rewards.internal.RewardNetworkImpl;
 import rewards.internal.account.AccountRepository;
@@ -21,23 +21,6 @@ public class RewardsConfig {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-	DataSource dataSource;
-
-	@Autowired  // This @Autowired annotation is optional here
-	public RewardsConfig(DataSource dataSource) {
-		this.dataSource = dataSource;
-	}
-
-    // TODO-10 (Optional) : Switch back to explicit `DataSource` configuration
-    // (Instead of using auto-configured DataSource, we are going to configure
-    //  our own DataSource bean. Normally we want to configure infra-structure
-    //  bean such as DataSource bean in a separate configuration class but
-    //  here for the sake of simplicity, we configure it along with application
-    //  beans.)
-    // - Uncomment @Bean method below
-    // - Remove the code above that performs DataSource injection
-    // - Fix compile errors in this code
-    /*
     @Bean
     public DataSource dataSource() {
         logger.debug("Creating the datasource bean explicitly");
@@ -48,7 +31,6 @@ public class RewardsConfig {
                         .addScript("classpath:data.sql")
                         .build();
     }
-    */
 
     @Bean
     public RewardNetwork rewardNetwork() {
@@ -60,19 +42,19 @@ public class RewardsConfig {
 
     @Bean
     public AccountRepository accountRepository() {
-        JdbcAccountRepository repository = new JdbcAccountRepository(dataSource);
+        JdbcAccountRepository repository = new JdbcAccountRepository(dataSource());
         return repository;
     }
 
     @Bean
     public RestaurantRepository restaurantRepository() {
-        JdbcRestaurantRepository repository = new JdbcRestaurantRepository(dataSource);
+        JdbcRestaurantRepository repository = new JdbcRestaurantRepository(dataSource());
         return repository;
     }
 
     @Bean
     public RewardRepository rewardRepository() {
-        JdbcRewardRepository repository = new JdbcRewardRepository(dataSource);
+        JdbcRewardRepository repository = new JdbcRewardRepository(dataSource());
         return repository;
     }
 
