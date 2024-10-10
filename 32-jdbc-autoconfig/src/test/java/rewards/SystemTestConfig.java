84a0b5a9ad806b62b7c2ecdad237ce0eed1d0b08
xls-codebase <93859139+xls-codebase@users.noreply.github.com>
2024-10-10 23:15:19 +0200

    Spring Boot - A Closer Look: Refactor: Optimize Imports

--- 32-jdbc-autoconfig/src/test/java/rewards/SystemTestConfig.java
+++ 32-jdbc-autoconfig/src/test/java/rewards/SystemTestConfig.java
@@ -3,7 +3,6 @@ package rewards;
 import config.RewardsConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
