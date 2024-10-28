a4c2b2525aca8ac5ee658340e9546345018ec6ca
xls-codebase <93859139+xls-codebase@users.noreply.github.com>
2024-10-28 11:01:46 +0100

    Spring Boot - Web Application with Spring Boot TODO-08: Add a new controller method

--- 36-mvc/src/main/java/accounts/web/AccountController.java
+++ 36-mvc/src/main/java/accounts/web/AccountController.java
@@ -3,6 +3,7 @@ package accounts.web;
 import accounts.AccountManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import rewards.internal.account.Account;
 
@@ -62,6 +63,10 @@ public class AccountController {
 	//   needed to retrieve an account.
 	// - Use the accountManager to obtain an account. This is the value to return
 	// - Save all work.
+	@GetMapping("/accounts/{entityId}")
+	public Account accountDetails(@PathVariable Long entityId) {
+		return accountManager.getAccount(entityId);
+	}
 
 
 	// TODO-10b: If AccountControllerTests.testHandleDetailsRequest()
