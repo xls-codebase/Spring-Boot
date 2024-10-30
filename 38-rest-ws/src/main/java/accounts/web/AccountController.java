c98ee58b51800edec1cb68a709b2ca421a5937a1
xls-codebase <93859139+xls-codebase@users.noreply.github.com>
2024-10-30 14:50:21 +0100

    Spring Boot - RESTful Application with Spring Boot TODO-10 - TODO-11: Make a REST endpoint from  addBeneficiary method

--- 38-rest-ws/src/main/java/accounts/web/AccountController.java
+++ 38-rest-ws/src/main/java/accounts/web/AccountController.java
@@ -119,14 +119,14 @@ public class AccountController {
 	// a. Respond to a POST /accounts/{accountId}/beneficiaries
 	// b. Extract a beneficiary name from the incoming request
 	// c. Indicate a "201 Created" status
-	public ResponseEntity<Void> addBeneficiary(long accountId, String beneficiaryName) {
-		
+	@PostMapping(value = "/accounts/{accountId}/beneficiaries")
+	public ResponseEntity<Void> addBeneficiary(@PathVariable("accountId") long accountId, @RequestBody String beneficiaryName) {
+		accountManager.addBeneficiary(accountId, beneficiaryName);
 		// TODO-11: Create a ResponseEntity containing the location of the newly
 		// created beneficiary.
 		// a. Use accountManager's addBeneficiary method to add a beneficiary to an account
 		// b. Use the entityWithLocation method - like we did for createAccount().
-		
-		return null;  // Modify this to return something
+		return entityWithLocation(beneficiaryName);
 	}
 
 	/**
