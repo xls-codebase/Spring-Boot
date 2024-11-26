6e034c37b3b9878a87f92a975bf63e68bcbbec36
xls-codebase <93859139+xls-codebase@users.noreply.github.com>
2024-11-26 16:42:39 +0100

    Spring Boot - Securing REST Application with Spring Security TODO-10: Enable method security

--- 42-security-rest/src/main/java/config/RestSecurityConfig.java
+++ 42-security-rest/src/main/java/config/RestSecurityConfig.java
@@ -3,6 +3,7 @@ package config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.core.userdetails.User;
@@ -18,6 +19,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 // - Add @EnableMethodSecurity annotation to this class
 
 @Configuration
+@EnableMethodSecurity
 public class RestSecurityConfig {
 
 	@Bean
