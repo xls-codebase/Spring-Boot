eb30f8a9bdf05602c991a347c84a6d67f64bb2b6
xls-codebase <93859139+xls-codebase@users.noreply.github.com>
2024-11-28 17:15:11 +0100

    Spring Boot - Securing REST Application with Spring Security TODO-18a (Optional - Create Custom AuthenticationProvider): Add authentication based upon the custom AuthenticationProvider

--- 42-security-rest/src/main/java/accounts/security/CustomAuthenticationProvider.java
+++ 42-security-rest/src/main/java/accounts/security/CustomAuthenticationProvider.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.stereotype.Component;
 
 //TODO-17 (Optional): Create custom AuthenticationProvider
 //- Note that it needs to implement AuthenticationProvider interface
@@ -16,6 +17,7 @@ import org.springframework.security.core.authority.AuthorityUtils;
 //TODO-18a (Optional): Add authentication based upon the custom AuthenticationProvider
 //- Annotate the class with @Component to make it a Spring manager bean
 
+@Component
 public class CustomAuthenticationProvider implements AuthenticationProvider {
 
 	@Override
