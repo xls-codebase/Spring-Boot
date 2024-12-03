28624f346c653f390855752e1c4db7edcdec16f6
xls-codebase <93859139+xls-codebase@users.noreply.github.com>
2024-12-03 18:50:18 +0100

    Spring Boot - Actuator TODO-16a: Implement RestaurantHealthCheck

--- 44-actuator/src/main/java/accounts/web/RestaurantHealthCheck.java
+++ 44-actuator/src/main/java/accounts/web/RestaurantHealthCheck.java
@@ -1,5 +1,10 @@
 package accounts.web;
 
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+import rewards.internal.restaurant.RestaurantRepository;
+
 /**
  * TODO-16a: Create custom health indicator
  * - Make this class implement HealthIndicator interface
@@ -9,8 +14,19 @@ package accounts.web;
  *   (no restaurants) or UP otherwise. (Note that RestaurantRepository
  *   has a method that returns number of restaurants.)
  */
-public class RestaurantHealthCheck {
+@Component
+public class RestaurantHealthCheck implements HealthIndicator {
 
+    private final RestaurantRepository restaurantRepository;
+
+    public RestaurantHealthCheck(RestaurantRepository restaurantRepository) {
+        this.restaurantRepository = restaurantRepository;
+    }
+
+    @Override
+    public Health health() {
+        return (restaurantRepository.getRestaurantCount() > 0) ? Health.up().build() : Health.down().build();
+    }
 }
 
 
